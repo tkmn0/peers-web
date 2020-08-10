5cc56fa03facff9f4628d574a5e4b8ffd9229ee3
nzer0 <tkmgin0914@gmail.com>
2020-08-10 20:13:28 +0900

    chore: lint

--- src/lib/logger/logHanlder.ts
+++ src/lib/logger/logHanlder.ts
@@ -11,19 +11,28 @@ export default class LogHandler {
 
   public log = (tag: string, message: string, args: any[] | null = null) => {
     if (this.level === "debug") {
-      console.log(`\u001b[42m[DEBUG] ${tag} - ${message}`, args == null ? "" : args);
+      console.log(
+        `\u001b[42m[DEBUG] ${tag} - ${message}`,
+        args == null ? "" : args
+      );
     }
   };
 
   public error = (tag: string, message: string, args: any[] | null = null) => {
     if (this.level === "error") {
-      console.error(`\u001b[41m[EROOR] ${tag} - ${message}`, args == null ? "" : args);
+      console.error(
+        `\u001b[41m[EROOR] ${tag} - ${message}`,
+        args == null ? "" : args
+      );
     }
   };
 
   public info = (tag: string, message: string, args: any[] | null = null) => {
     if (this.level === "info") {
-      console.log(`\u001b[44m[INFO] ${tag} - ${message}`, args == null ? "" : args);
+      console.log(
+        `\u001b[44m[INFO] ${tag} - ${message}`,
+        args == null ? "" : args
+      );
     }
   };
 }
